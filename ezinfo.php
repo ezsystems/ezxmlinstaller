2558c79daab7c46eaded25f68f916862ffaf19b1
Bertrand Dunogier <bd@ez.no>
2011-02-18 16:25:38 +0100

    Updated version number to use autogen + copyright

--- ezinfo.php
+++ ezinfo.php
@@ -28,8 +28,8 @@ class ezxmlinstallerInfo
     public static function info()
     {
         return array( 'Name' => "eZ Publish XML Installer",
-                      'Version' => "1.0.0",
-                      'Copyright' => "Copyright (C) 1999-2010 eZ Systems AS",
+                      'Version' => "//autogentag//",
+                      'Copyright' => "Copyright (C) 1999-2011 eZ Systems AS",
                       'License' => "GNU General Public License v2.0"
                      );
     }
