276beb04f192ce47a78ef7a72e369d5c848bd762
Damien Pobel <dpobel@free.fr>
2011-03-09 08:17:34 +0100

    Localization fre-FR : added missing translations

--- translations/fre-FR/translation.ts
+++ translations/fre-FR/translation.ts
@@ -1,20 +1,22 @@
-<!DOCTYPE TS><TS>
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
 <context>
     <name>design/standard/class/datatype</name>
     <message>
         <source>Template Location</source>
-        <translation type="unfinished"></translation>
+        <translation>Emplacement du modèle</translation>
     </message>
     <message>
         <source>Empty</source>
-        <translation type="unfinished"></translation>
+        <translation>Vide</translation>
     </message>
 </context>
 <context>
     <name>extension/ezxmkinstaller</name>
     <message>
         <source>XML Publisher</source>
-        <translation type="unfinished"></translation>
+        <translation>Publieur XML</translation>
     </message>
 </context>
 <context>
@@ -22,7 +24,7 @@
     <message>
         <source>Feature Select</source>
         <comment>Datatype name</comment>
-        <translation type="unfinished"></translation>
+        <translation>Sélection de fonctionnalité</translation>
     </message>
 </context>
 </TS>
